3313224ce28ddf4e82d0aa1fa34a7d14dc8c3522
mishra <s0prmish@uni-bonn.de>
2021-05-04 01:22:41 +0200

    add initial tests

---
+++ tflow.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import tensorflow_hub as hub
+
+print("TF Version: ", tf.__version__)
+print("TF-Hub version: ", hub.__version__)
+print("Eager mode enabled: ", tf.executing_eagerly())
+print("GPU available: ", tf.test.is_gpu_available())
